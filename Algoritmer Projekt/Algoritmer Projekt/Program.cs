899cd4a8f743c8b5bfcaa8a7dba5f876b221769b
Lasse Agerskov <lasse.agerskov@gmail.com>
2026-02-05 12:04:12 +0100

    Solution oprettet

---
+++ Algoritmer Projekt/Algoritmer Projekt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Algoritmer_Projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
