c28172080bf5625d5cbcb10789803e929318783f
Lasse Agerskov <lasse.agerskov@gmail.com>
2026-02-09 09:22:55 +0100

    ..

--- source/Del 2 - GraphAndSearch/Node.cs
+++ source/Del 2 - GraphAndSearch/Node.cs
@@ -4,9 +4,25 @@ using System.Text;
 
 namespace GraphAndSearch
 {
+    /// <summary>
+    /// Forlystelser
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Node<T>
     {
-        private T value;
-        private List<Edge<T>> edges;
+        public T Value { get; }
+        private List<Edge<T>> _edges;
+
+        public Node(T value, Node<T> parent) 
+        {
+            Edge<T> edge = new Edge<T>(this, parent);
+            Value = value;
+            _edges = new List<Edge<T>>();
+        }
+        public void AddEdge(Edge<T> edge)
+        {
+            _edges.Add(edge);
+        }
     }
 }
+    
--- source/Del 2 - GraphAndSearch/Program.cs
+++ source/Del 2 - GraphAndSearch/Program.cs
@@ -1,10 +1,35 @@
 ﻿namespace GraphAndSearch
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Graph<string> graph = new Graph<string>();
+            graph.AddNode("Entrance"); 
+            graph.AddNode("Carousel");
+            graph.AddNode("Mini Train");
+            graph.AddNode("Ice Cream");
+            graph.AddNode("Roller Coaster");
+            graph.AddNode("Haunted House");
+            graph.AddNode("Water Ride");
+            graph.AddNode("Pirate Ship");
+            graph.AddNode("Climbing Tower");
+            graph.AddNode("Volcano Ride");
+
+            graph.AddEdges("Entrance", "Carousel");
+            graph.AddEdges("Entrance", "Mini Train");
+            graph.AddEdges("Entrance", "Ice Cream");
+            graph.AddEdges("Entrance", "Ice Cream");
+            graph.AddEdges("Entrance", "Ice Cream");
+            graph.AddEdges("Entrance", "Ice Cream");
+            graph.AddEdges("Entrance", "Ice Cream");
+            graph.AddEdges("Entrance", "Ice Cream");
+            graph.AddEdges("Entrance", "Ice Cream");
+            graph.AddEdges("Entrance", "Ice Cream");
+            graph.AddEdges("Entrance", "Ice Cream");
+            graph.AddEdges("Entrance", "Ice Cream");
+            graph.AddEdges("Entrance", "Ice Cream");
+
         }
     }
 }
