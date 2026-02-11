d1a0e6a8f9299540123363e064b40d96fded503e
Matias Heiberg Nielsen <perm_diners7z@icloud.com>
2026-02-11 11:30:58 +0100

    slettede unødvendige count++

--- source/Del 2 - GraphAndSearch/Edge.cs
+++ source/Del 2 - GraphAndSearch/Edge.cs
@@ -10,13 +10,13 @@ namespace GraphAndSearch
     /// <typeparam name="T"></typeparam>
     public class Edge<T>
     {
-        private Node<T> _node1 { get; }
-        private Node<T> _node2 { get; }
+        private Node<T> _child { get; }
+        private Node<T> _parent { get; }
 
         public Edge(Node<T> childNode, Node<T> parentNode)
         {
-            _node1 = childNode;
-            _node2 = parentNode;
+            _child = childNode;
+            _parent = parentNode;
         }
     }
 }
